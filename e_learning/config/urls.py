e7daeb5b575c4a6aa9b34e6828791cb59528d1b2
Abdusamad Malikov <sevbofx@gmail.com>
2023-01-03 15:56:25 +0500

    `urls`  config updated

--- e_learning/config/urls.py
+++ e_learning/config/urls.py
@@ -1,9 +1,11 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
+from apps.accounts.views import RegisterView
+
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -16,6 +18,8 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/v1/auth/register/', RegisterView.as_view(), name='register'),
+    path('api/v1/accounts/', include('apps.accounts.urls'), name='accounts'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='swagger'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='redoc'),
 ]
