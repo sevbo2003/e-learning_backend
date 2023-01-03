e121fce5ff30c081a8ee84e5758007fc211c3094
Abdusamad Malikov <sevbofx@gmail.com>
2023-01-03 16:00:27 +0500

    `JWT` auth implemented

--- e_learning/config/urls.py
+++ e_learning/config/urls.py
@@ -5,6 +5,7 @@ from django.urls import path, include
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from apps.accounts.views import RegisterView
+from rest_framework_simplejwt.views import (TokenObtainPairView, TokenRefreshView, TokenVerifyView)
 
 
 schema_view = get_schema_view(
@@ -19,6 +20,9 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/auth/register/', RegisterView.as_view(), name='register'),
+    path('api/v1/auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/v1/auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/v1/auth/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
     path('api/v1/accounts/', include('apps.accounts.urls'), name='accounts'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='swagger'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='redoc'),
