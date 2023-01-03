c783bb7a448f48ec053b15b5ea8331c5c3fefd49
Abdusamad Malikov <sevbofx@gmail.com>
2023-01-03 15:56:08 +0500

    `RegisterView` created

--- e_learning/apps/accounts/views.py
+++ e_learning/apps/accounts/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from rest_framework import viewsets, views
+from rest_framework.response import Response
+from apps.accounts.models import User, Student, Teacher, UserTypes
+from apps.accounts.serializers import UserSerializer, StudentSerializer, TeacherSerializer
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import status
 
-# Create your views here.
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
+    http_method_names = ['get', 'put', 'patch', 'delete']
+
+
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+        return Response(serializer.data)
+
+    def perform_update(self, serializer):
+        serializer.save()
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    def perform_destroy(self, instance):
+        instance.delete()
+
+
+class RegisterView(views.APIView):
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
